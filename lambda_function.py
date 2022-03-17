a7b01a31e4683215d09b83fe6bb9341be330218c
Dayyapu(Consultant), Shiva <sdayyapu@qti.qualcomm.com>
2022-03-17 12:43:48 -0700

    update from gitHub

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,8 @@
-import json
+import pandas as pd
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda -  from GitHub!')
-    }
+    d = {'col1':[1,2],'col2':[3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Hello From gitHub!')
+
